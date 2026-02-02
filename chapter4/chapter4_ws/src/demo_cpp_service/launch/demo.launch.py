33e681e5773f85cdbd71930c2fa73f291b2f2814
shenhang <245100189@qq.com>
2026-02-02 10:58:58 +0800

    update

--- chapter4/chapter4_ws/src/demo_cpp_service/launch/demo.launch.py
+++ chapter4/chapter4_ws/src/demo_cpp_service/launch/demo.launch.py
@@ -2,11 +2,15 @@ import launch
 import launch_ros
 
 def generate_launch_description(): #函数名是写死的
+    #1.声明一个launch参数
+    action_declare_arg_background_g = launch.actions.DeclareLaunchArgument('launch_arg_bg',default_value='150')
+    #2.把launch的参数手动传递给某个节点
     """"产生launch描述"""
     #ros2 run turtlesim turtlesim_node
     action_node_turtlesim_node = launch_ros.actions.Node(
         package='turtlesim',
         executable='turtlesim_node',
+        parameters=[{'background_g': launch.substitutions.LaunchConfiguration('launch_arg_bg',default='150')}],
         output='screen'
     )
 

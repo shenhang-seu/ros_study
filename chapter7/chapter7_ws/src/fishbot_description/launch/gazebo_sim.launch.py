1c3f20454b06143ddefa7cbb7d2aee5a0c1081b7
shenhang <245100189@qq.com>
2026-02-09 15:08:50 +0800

    机器人导航介绍

---
+++ chapter7/chapter7_ws/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,85 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    #获取默认的路径
+    #/home/shenhang/my_github/ros_study/chapter6/chapter6_ws/src/fishbot_description/urdf/fishbot/fishbot.urdf.xacro
+    urdf_package_path = get_package_share_directory('fishbot_description')
+    default_xacro_path = os.path.join(urdf_package_path,'urdf/fishbot/','fishbot.urdf.xacro')
+    #/home/shenhang/my_github/ros_study/chapter6/chapter6_ws/src/fishbot_description/world/custom_room.world
+    default_gazebo_world_path = os.path.join(urdf_package_path,'world','custom_room.world')
+
+    #声明一个urdf目录的参数, 方便修改
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model',default_value=str(default_xacro_path),description='加载的模型文件路径'
+    )
+
+    #通过模型文件路径, 获取内容, 并转换成参数值对象, 以供传入robot_state_publisher节点的启动
+    substitutions_command_result = launch.substitutions.Command(['xacro ',launch.substitutions.LaunchConfiguration('model')])
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(substitutions_command_result,value_type=str)
+
+    #ros2 run robot_state_publisher robot_state_publisher robot_description xxx
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description_value}]#/robot_description是robot_state_publisher节点会创建发布的话题(话题名是固定的)
+    )
+
+    #ros2 launch gazebo_ros gazebo.launch.py world:=xxx.world
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'),'/launch','/gazebo.launch.py']
+        ),
+        launch_arguments=[('world',default_gazebo_world_path),('verbose','true')]
+    )
+   
+    #把机器人加载到gazebo中
+    #ros2 run gazebo_ros spawn_entity.py
+    action_spawn_entity = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','/robot_description','-entity','fishbot']
+    )
+
+    action_load_joint_state_broadcaster_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_joint_state_broadcaster --set-state active'.split(' '),
+        output='screen'
+    )
+
+    action_load_effort_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_effort_controller --set-state active'.split(' '),
+        output='screen'
+    )
+
+    action_load_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_diff_drive_controller --set-state active'.split(' '),
+        output='screen'
+    )
+
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        action_launch_gazebo,
+        action_spawn_entity,
+        #等机器人加载到gazebo成功后再开始加载控制器
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_spawn_entity,
+                on_exit=[action_load_joint_state_broadcaster_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_load_joint_state_broadcaster_controller,
+                on_exit=[action_load_effort_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_load_effort_controller,
+                on_exit=[action_load_diff_drive_controller],
+            )
+        ),
+    ])
